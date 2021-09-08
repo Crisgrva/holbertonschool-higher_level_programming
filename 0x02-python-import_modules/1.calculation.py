bbfe81eb4d6792ca4010d67b63e679b28b77371b
Crisgrva <crstngranada@gmail.com>
2021-09-08 13:41:14 -0500

    fix 1 and 2

--- 0x02-python-import_modules/1.calculation.py
+++ 0x02-python-import_modules/1.calculation.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-calc = __import__("calculator_1")
+if __name__ == "__main__":
+    from calculator_1 import add
+    from calculator_1 import sub
+    from calculator_1 import mul
+    from calculator_1 import div
+    
+    a = 10
+    b = 5
 
-a = 10
-b = 5
-
-print("{:d} + {:d} = {:d}".format(a, b, calc.add(a, b)))
-print("{:d} - {:d} = {:d}".format(a, b, calc.sub(a, b)))
-print("{:d} * {:d} = {:d}".format(a, b, calc.mul(a, b)))
-print("{:d} / {:d} = {:d}".format(a, b, calc.div(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-argvlen = len(sys.argv)
+    argvlen = len(sys.argv)
 
-if argvlen == 2:
-    nargv = " argument:"
-else:
-    nargv = " arguments:"
+    if argvlen == 2:
+        nargv = " argument:"
+    else:
+        nargv = " arguments:"
 
-print("{:d}".format(len(sys.argv) - 1) + nargv)
+    print("{:d}".format(len(sys.argv) - 1) + nargv)
 
-for arguments in range(1, argvlen):
-    print("{:d}: {}".format(arguments, sys.argv[arguments]))
+    for arguments in range(1, argvlen):
+        print("{:d}: {}".format(arguments, sys.argv[arguments]))
