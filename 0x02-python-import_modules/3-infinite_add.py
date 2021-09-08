f5b33fc35bee63fa995d2dd9b4ad8560ca501124
Crisgrva <crstngranada@gmail.com>
2021-09-08 13:48:19 -0500

    fix task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
-"""
-Declare Variable
-"""
-argvlen = len(sys.argv)
-result = 0
+if __name__ == "__main__":
+    import sys
+    """
+    Declare Variable
+    """
+    argvlen = len(sys.argv)
+    result = 0
 
-for num in range(1, argvlen):
-    result += int(sys.argv[num])
+    for num in range(1, argvlen):
+        result += int(sys.argv[num])
 
-print("{:d}".format(result))
+    print("{:d}".format(result))
