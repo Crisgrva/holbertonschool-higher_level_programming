c64dca0240b4f4de046ddf801bc2808059bdba5e
Cristian Granada <crstngranada@gmail.com>
2021-09-08 13:33:53 -0700

    fix 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     argc = len(sys.argv)
 
     if argc != 4:
-        print("Usage: ./100-my-calculator.py <a> <operator> <b>")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
     sign = sys.argv[2]
