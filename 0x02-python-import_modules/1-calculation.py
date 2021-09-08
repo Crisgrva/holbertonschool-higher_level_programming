d9e06e08f1e257c3218993b0c79c0225c4b736d4
Crisgrva <crstngranada@gmail.com>
2021-09-08 13:45:07 -0500

    delete excess imports

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add
-    from calculator_1 import sub
-    from calculator_1 import mul
-    from calculator_1 import div
+    import calculator_1 as calc
     a = 10
     b = 5
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, calc.add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, calc.sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, calc.mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, calc.div(a, b)))
