5a364cc263c75282e4ed41ca478bd9e8b43150ef
Crisgrva <crstngranada@gmail.com>
2021-09-07 22:49:49 -0500

    add last whitespace at final

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,13 +2,10 @@
 def fizzbuzz():
     for x in range(1, 101):
         if x % 15 == 0:
-            print("{}".format("FizzBuzz"), end="")
+            print("{}".format("FizzBuzz "), end="")
         elif x % 5 == 0:
-            print("{}".format("Buzz"), end="")
+            print("{}".format("Buzz "), end="")
         elif x % 3 == 0:
-            print("{}".format("Fizz"), end="")
+            print("{}".format("Fizz "), end="")
         else:
-            print("{:d}".format(x), end="")
-
-        if x != 100:
-            print("{}".format(" "), end="")
+            print("{:d} ".format(x), end="")
