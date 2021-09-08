918ea82eedd21094fa1426a0eda16839b1143078
Crisgrva <crstngranada@gmail.com>
2021-09-07 20:57:00 -0500

    add task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for x in range(0, 10):
+    for i in range((x + 1), 10):
+        if (x == 8 and i == 9):
+            print("{:d}{:d}".format(x, i), end='')
+        else: 
+            print("{:d}{:d}, ".format(x, i), end='')
