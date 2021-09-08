7ddb2c55cae56e6e0e1d8bd3be02f801e4ec94ae
Crisgrva <crstngranada@gmail.com>
2021-09-07 20:59:14 -0500

    add newline to last number

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for x in range(0, 10):
     for i in range((x + 1), 10):
         if (x == 8 and i == 9):
-            print("{:d}{:d}".format(x, i), end='')
+            print("{:d}{:d}".format(x, i))
         else:
             print("{:d}{:d}, ".format(x, i), end='')
