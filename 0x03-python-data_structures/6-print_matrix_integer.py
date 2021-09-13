7cbe65983a677728a50ab6b222fcd12f54365001
Crisgrva <crstngranada@gmail.com>
2021-09-13 16:18:07 -0500

    add task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for list in matrix:
+        for element in range(0, len(list)):
+            print("{}".format(list[element]), end="")
+            if element != len(list) - 1:
+                print("{}".format(" "), end="")
+        print()
