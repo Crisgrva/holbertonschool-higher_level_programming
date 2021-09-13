4fac3179d494e0a32fedfd154a43b8141bf37551
Crisgrva <crstngranada@gmail.com>
2021-09-13 15:43:55 -0500

    print None when element is None

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     for i in reversed(range(len(my_list))):
-        if my_list[i] == None:
-            print("None")
-        else:
-            print("{:d}".format(my_list[i]))
+        print("{}".format(my_list[i]))
