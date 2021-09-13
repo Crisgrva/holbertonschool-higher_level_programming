b86ee5c2f5884929e6080defcc1063ee5b0c3904
Crisgrva <crstngranada@gmail.com>
2021-09-13 17:39:44 -0500

    add task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -7,6 +7,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
         num_1 = tuple_a[0]
     except IndexError:
         num_1 = tuple_b[0]
+    except IndexError:
+        num_1 = 0
 
     try:
         num_2 = tuple_a[1] + tuple_b[1]
@@ -14,6 +16,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
         num_2 = tuple_a[1]
     except IndexError:
         num_2 = tuple_b[1]
+    except IndexError:
+        num_2 = 0
 
     new_tuple = (num_1, num_2)
     return new_tuple
