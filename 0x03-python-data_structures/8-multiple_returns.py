5a054cb7d5b190dda258b79ff87f420563a72073
Crisgrva <crstngranada@gmail.com>
2021-09-13 19:33:03 -0500

    add task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     sentenceLen = len(sentence)
-    fstLetter = sentence[0]
+    if sentenceLen == 0:
+        fstLetter = None
+    else:
+        fstLetter = sentence[0]
     new_tuple = (sentenceLen, fstLetter)
     return new_tuple
