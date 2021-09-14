b6e24055092197c0c61e977762437b3e2886ecdc
Crisgrva <crstngranada@gmail.com>
2021-09-13 19:15:01 -0500

    fix task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -29,6 +29,13 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     if len(tuple_a) == 0 and len(tuple_b) == 2:
         return tuple_b
+    
+    if len(tuple_a) == 1 and len(tuple_b) == 0:
+        new_duple = (tuple_a[0], 0)
+        return new_duple
+
+    if len(tuple_a) == 0 and len(tuple_b) == 1:
+        new_duple = (0, tuple_b[0])
 
     new_duple = (0, 0)
     return new_duple
