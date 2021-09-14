bdba6fbb44dec08959971285fdc4367761591b80
Crisgrva <crstngranada@gmail.com>
2021-09-13 19:26:10 -0500

    pycodestyle

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
 
-    if len(tuple_a) == 2 and len(tuple_b) == 2:
+    if len(tuple_a) >= 2 and len(tuple_b) >= 2:
         num_1 = tuple_a[0] + tuple_b[0]
         num_2 = tuple_a[1] + tuple_b[1]
         new_duple = (num_1, num_2)
@@ -36,6 +36,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     if len(tuple_a) == 0 and len(tuple_b) == 1:
         new_duple = (tuple_b[0], 0)
+        return new_duple
 
     new_duple = (0, 0)
     return new_duple
--- 0x03-python-data_structures/7-main.py
+++ 0x03-python-data_structures/7-main.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 add_tuple = __import__('7-add_tuple').add_tuple
 
-tuple_a = (1, 89)
-tuple_b = (88, 11)
-new_tuple = add_tuple(tuple_a, tuple_b)
-print(new_tuple)
-
-print(add_tuple(tuple_a, (1, )))
-print(add_tuple(tuple_a, ()))
+print(add_tuple((1, 2), (3, 4)))
+print(add_tuple((1, 2), (3, )))
+print(add_tuple((1, 2), ()))
+print(add_tuple((1, ), (3, )))
+print(add_tuple((1, ), ()))
+print(add_tuple((1, ), (3, 4)))
+print(add_tuple((), (3, 4)))
+print(add_tuple((), (3, )))
+print(add_tuple((), ()))
+print(add_tuple((1, 2, 3, 4), (5, 6, 7, 8)))
