ff042b10f045ab217b32b1359b5f5853294c34a7
Crisgrva <crstngranada@gmail.com>
2021-09-15 19:36:41 -0500

    try task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    romans = {
+           "I": 1,
+           "V": 5,
+           "X": 10,
+           "L": 50,
+           "C": 100,
+           "D": 500,
+           "M": 1000,
+           }
+    result = 0
+    fst_num = romans.get(roman_string[0])
+    for x in range(len(roman_string)):
+        print(int(romans.get(roman_string[x])))
+        if fst_num < int(romans.get(roman_string[x])):
+            result = romans.get(roman_string[x]) - result
+        else:
+            result += romans.get(roman_string[x])
+    return result
