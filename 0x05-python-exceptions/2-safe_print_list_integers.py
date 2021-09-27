ca2be7190e43d239642264f0cc3d35169ab19bc4
Crisgrva <crstngranada@gmail.com>
2021-09-27 12:53:29 -0500

    try task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    
+    count = 0
+
+    for num in my_list[:x]:
+        try:
+            print("{:d}".format(num), end="")
+            count += 1
+        except (TypeError, ValueError):
+            continue
+
+    print()
+    return count
