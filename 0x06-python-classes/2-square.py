afd1358cb8dff97e128cf87f4a4922a53bdfb9b5
Crisgrva <crstngranada@gmail.com>
2021-09-28 17:34:05 -0500

    fix task 1

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""1. Square with size"""
+
+
+class Square:
+    """Write a class Square that defines a square by:
+        (based on 0-square.py)"""
+    def __init__(self, size=0):
+        self.__size = size
+        if size is not int:
+            raise Exception("size must be an integer")
+        elif size < 0:
+            raise Exception("size must be >= 0")
