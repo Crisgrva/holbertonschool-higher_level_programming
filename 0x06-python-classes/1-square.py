54d6063d931c845070e3a3fdd57992f36af850a4
Crisgrva <crstngranada@gmail.com>
2021-09-28 16:52:01 -0500

    add task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""1. Square with size"""
+
+
+class Square:
+    """Write a class Square that defines a square by:
+        (based on 0-square.py)"""
+    def __init__(self, size=0):
+        self.__size = size
