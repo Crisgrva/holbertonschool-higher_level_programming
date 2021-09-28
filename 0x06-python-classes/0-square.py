796549af9977fa42865963148779044e1442a3ee
Crisgrva <crstngranada@gmail.com>
2021-09-28 16:42:48 -0500

    try comments in python

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+"""
+0. My first square
+"""
+
+
 class Square:
     """Empty class Square that defines a square.
     Starting to use Comments in Python
     """
-Square.__dict__
-"""
-atributtes
-"""
