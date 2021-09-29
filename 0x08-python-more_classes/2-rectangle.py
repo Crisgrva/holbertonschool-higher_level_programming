9820019e2ab5dbd5e7595a3129eb803029db762d
Crisgrva <crstngranada@gmail.com>
2021-09-29 17:42:23 -0500

    add task 2

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+1. Real definition of a rectangle
+"""
+
+
+class Rectangle:
+    """Empty class Square that defines a rectangle.
+    Starting to use Comments in Python
+    """
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if isinstance(value, int) is False:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if isinstance(value, int) is False:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return(self.height * self.width)
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return(0)
+        return ((self.width * 2) + (self.height * 2))
