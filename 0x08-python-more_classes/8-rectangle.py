40761da7e899589e3cd294e8c62ea5bd5c998c61
Crisgrva <crstngranada@gmail.com>
2021-09-30 11:34:17 -0500

    add if rect 1 is iqual rect 2

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -49,7 +49,9 @@ class Rectangle:
         elif isinstance(rect_2, Rectangle) is False:
             raise TypeError("rect_2 must be an instance of Rectangle")
 
-        if rect_1.area() > rect_2.area():
+        if rect_1.area() == rect_2.area():
+            return rect_1
+        elif rect_1.area() > rect_2.area():
             return rect_1
         else:
             return rect_2
