6ae3adc654577b7b93a26e25679443a086611e83
Crisgrva <crstngranada@gmail.com>
2021-09-30 12:13:23 -0500

    add task 9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -12,8 +12,8 @@ class Rectangle:
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
