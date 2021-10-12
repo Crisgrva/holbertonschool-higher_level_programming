7e1f7693106033d2737ebf1b5f9339de69e7e371
Crisgrva <crstngranada@gmail.com>
2021-10-11 23:38:56 -0500

    add test task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,8 +8,6 @@ class MyList(list):
     """
     Write a class MyList that inherits from list
     """
-    def __init__(self):
-        super().__init__()
 
     def print_sorted(self):
         print(sorted(self))
