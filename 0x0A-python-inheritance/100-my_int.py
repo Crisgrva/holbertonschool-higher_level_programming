84b91e8bbe0e21a90d30fc4e7faafa40129b735e
Crisgrva <crstngranada@gmail.com>
2021-10-11 23:58:09 -0500

    last commit

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+12. My integer
+"""
+
+
+class MyInt(int):
+    """
+    Write a class MyList that inherits from list
+    """
+    def __init__(self):
+        super().__init__()
