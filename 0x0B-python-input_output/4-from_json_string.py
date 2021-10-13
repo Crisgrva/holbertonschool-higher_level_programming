d1b72ccd74e3021cf093aa148cf4a8a021a2f78c
Crisgrva <crstngranada@gmail.com>
2021-10-12 19:02:12 -0500

    fix task 4

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -12,4 +12,4 @@ def from_json_string(my_str):
     Write a function that returns an object
     (Python data structure) represented by a JSON string:
     """
-    return json.load(my_str)
+    return json.loads(my_str)
