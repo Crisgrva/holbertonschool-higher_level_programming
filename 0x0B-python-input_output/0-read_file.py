16de37f207650327398aba2fd8ac628231440275
Crisgrva <crstngranada@gmail.com>
2021-10-12 19:00:07 -0500

    fix task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,9 @@
 
 
 def read_file(filename=""):
+    """
+    Write a function that reads a text file
+    (UTF8) and prints it to stdout:
+    """
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
-
---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+4. From JSON string to Object
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    Write a function that returns an object
+    (Python data structure) represented by a JSON string:
+    """
+    return json.load(my_str)
