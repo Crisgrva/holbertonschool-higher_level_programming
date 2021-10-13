8de64913aa861ca4e2b277014752ba656209cd62
Crisgrva <crstngranada@gmail.com>
2021-10-12 21:13:19 -0500

    add task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+11. Student to disk and reload
+"""
+
+
+class Student:
+    """
+    Write a class Student that defines a
+    student by: (based on 10-student.py)
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return(self.__dict__)
+
+        return({item: self.__dict__[item] for item
+                in attrs if item in self.__dict__})
+
+    def reload_from_json(self, json):
+        self.first_name = json.get("first_name")
+        self.last_name = json.get("last_name")
+        self.age = json.get("age")
