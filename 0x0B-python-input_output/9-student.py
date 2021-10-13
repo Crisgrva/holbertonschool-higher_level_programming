b6b0dc2c5781d975724ad217691faf5289b09772
Crisgrva <crstngranada@gmail.com>
2021-10-12 20:06:24 -0500

    try task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+9. Student to JSON
+"""
+
+
+class Student:
+    """
+    Write a class Student that defines a student by:
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return(self.__dir__)
