829bc2f93892883da9862b74ee6e87990d5cf349
Crisgrva <crstngranada@gmail.com>
2021-10-19 03:40:37 -0500

    add task 9

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+10. And now, the Square!
+"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    the class Square that inherits from Rectangle:
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Class Constructor
+        """
+        self.size = size
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """overloading __str__"""
+
+        return "[Square] {} {}/{} - {}"\
+            .format(id, self.x, self.y, self.size)
