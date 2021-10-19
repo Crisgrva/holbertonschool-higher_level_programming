064f33d6ad234d905055fc75421f8b9c8d572061
Crisgrva <crstngranada@gmail.com>
2021-10-19 04:01:39 -0500

    add task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -37,3 +37,29 @@ class Square(Rectangle):
 
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """
+        1st argument should be the id attribute
+        2nd argument should be the size attribute
+        3rd argument should be the x attribute
+        4th argument should be the y attribute
+        """
+        if len(args) > 0:
+            try:
+                self.id = args[0]
+                self.size = args[1]
+                self.x = args[2]
+                self.y = args[3]
+            except Exception:
+                pass
+        else:
+            _id = kwargs.get("id")
+            _size = kwargs.get("size")
+            _x = kwargs.get("x")
+            _y = kwargs.get("y")
+
+            self.id = _id if _id is not None else self.id
+            self.size = _size if _size is not None else self.size
+            self.x = _x if _x is not None else self.x
+            self.y = _y if _y is not None else self.y
