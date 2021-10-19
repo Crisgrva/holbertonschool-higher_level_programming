1dd4881b3ccd21f23fcb1726e7e9bcad4e1902b3
Crisgrva <crstngranada@gmail.com>
2021-10-19 03:51:42 -0500

    add task 10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -24,3 +24,16 @@ class Square(Rectangle):
 
         return "[Square] ({}) {}/{} - {}"\
             .format(self.id, self.x, self.y, self.size)
+
+    @property
+    def size(self):
+        """Getter method for size"""
+
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter method for size"""
+
+        self.width = value
+        self.height = value
