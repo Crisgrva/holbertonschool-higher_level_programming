b25d23d3d7c3031fcbf9f7008d06cb22a2a01ca2
Crisgrva <crstngranada@gmail.com>
2021-11-16 21:22:50 +0000

    try task 5

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,8 @@
 -- Script that creates a table called first_table in the current database in your MySQL server.
 CREATE TABLE IF NOT EXISTS `first_table`
 (
-    `id` INT,
-    `name` VARCHAR(256)
-);
+    `id` INT DEFAULT NULL,
+    `name` VARCHAR(256) DEFAULT NULL
+)
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci 
+;
