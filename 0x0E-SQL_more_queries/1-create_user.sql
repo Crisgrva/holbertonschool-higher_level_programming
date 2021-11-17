72bf9f5e5b4dc07a1b57699519a500b6305502ee
Crisgrva <crstngranada@gmail.com>
2021-11-17 17:00:44 +0000

    fix task 0 and add task 1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Script that creates the MySQL server user user_0d_1.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
