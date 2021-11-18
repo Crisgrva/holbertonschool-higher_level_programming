941b5b7528497d90099e0df5e39db4700feca985
Crisgrva <crstngranada@gmail.com>
2021-11-18 03:25:34 +0000

    try task 100

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -4,3 +4,4 @@ LEFT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
 LEFT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_shows.title != "Dexter"
 GROUP BY tv_genres.name
+ORDER BY tv_genres.name ASC
