0969c34870e8e4894ef0b5bc68da526073c27d24
Cristian Granada <crisgrvc@Cristians-MacBook-Pro.local>
2021-12-13 12:48:34 -0500

    add if statements

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,10 @@
-#!/usr/bin/node
+#!/usr/local/bin/node
 const { argv } = require('process');
 const newArray = [];
-Array.from(argv).slice(2).forEach(element =>
-  newArray.push(parseInt(element)));
+if (argv.length > 3) {
+  Array.from(argv).slice(2).forEach(element =>
+    newArray.push(parseInt(element)));
 
-console.log(newArray.sort(
-  function (a, b) { return a - b; })[newArray.length - 2]);
+  console.log(newArray.sort(
+    function (a, b) { return a - b; })[newArray.length - 2]);
+} else { console.log(0); }
