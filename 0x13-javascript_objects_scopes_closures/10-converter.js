2ea88dda5c6632e585da5b4b5e9fbd87202917da
Crisgrva <crstngranada@gmail.com>
2021-12-14 18:08:02 +0000

    fix task 10

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.converter = function (base) {
   function baseConvert (number) {
-    return number.toString(number, base);
+    return number.toString(base);
   }
   return baseConvert;
 };
--- 0x13-javascript_objects_scopes_closures/10-main.js
+++ 0x13-javascript_objects_scopes_closures/10-main.js
@@ -1,15 +1,13 @@
 #!/usr/bin/node
 const converter = require('./10-converter').converter;
-const num = 12;
-console.log(num.toString(16));
-// let myConverter = converter(10)
+let myConverter = converter(10);
 
-// console.log(myConverter(2))
-// console.log(myConverter(12))
-// console.log(myConverter(89))
+console.log(myConverter(2));
+console.log(myConverter(12));
+console.log(myConverter(89));
 
-// myConverter = converter(16)
+myConverter = converter(16);
 
-// console.log(myConverter(2))
-// console.log(myConverter(12))
-// console.log(myConverter(89))
+console.log(myConverter(2));
+console.log(myConverter(12));
+console.log(myConverter(89));
