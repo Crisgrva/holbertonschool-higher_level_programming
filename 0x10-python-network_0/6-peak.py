8f2d4a01fcfbbb7e5b8a9b5b716ef8681eb00030
Crisgrva <crstngranada@gmail.com>
2022-01-09 20:07:40 -0500

    [ Task 6 ] Technical interview preparation [ 01/09/2022/08:07 ]

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Technical interview preparation:"""
+
+
+def find_peak(list_of_integers):
+    """function that finds a peak in a list of unsorted integers."""
+    if len(list_of_integers) > 0:
+        list_of_integers.sort()
+        return list_of_integers[-1]
+    return None
