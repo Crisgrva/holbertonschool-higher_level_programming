406cb55199cf2e99a9d934c871b86ff9b4055f54
Crisgrva <crstngranada@gmail.com>
2022-01-11 13:22:08 -0500

    [ Task 4 ] Add script that fetch from url with request module [ 01/11/2022/01:22 ]

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Write a Python script that fetches https://intranet.hbtn.io/status
+"""
+
+import requests
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+    response = requests.get(url)
+    print("Body response:")
+    print(f"\t- type: {type(response.text)}")
+    print(f"\t - content: {response.text}")
