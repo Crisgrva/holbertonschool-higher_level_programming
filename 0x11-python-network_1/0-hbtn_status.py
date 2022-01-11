3029c7917d11d820d39c8df25eb9380244193fb0
Crisgrva <crstngranada@gmail.com>
2022-01-11 10:56:48 -0500

    [ Task 0 ] Try another way [ 01/11/2022/10:56 ]

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 """
-Write a Python script that
-fetches from https://intranet.hbtn.io/status
+Python script that fetches https://intranet.hbtn.io/status
 """
-
+from urllib import request
 
 if __name__ == "__main__":
-    from urllib import request
-    url = "https://intranet.hbtn.io/status"
-
-    with request.urlopen(url) as response:
-        html = response.read()
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
         print("Body response:")
-        print(f"    - type: {type(html)}")
-        print(f"    - content: {html}")
-        print(f"    - utf8 content: {html.decode('utf-8')}")
+        print("\t- type:", type(content))
+        print("\t- content:", content)
+        print("\t- utf8 content:", content.decode('utf8'))
