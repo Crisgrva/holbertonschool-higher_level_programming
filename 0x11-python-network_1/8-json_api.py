dbf29e127393a67380eac8f0a6b02975fecacbe5
Crisgrva <crstngranada@gmail.com>
2022-01-11 14:27:11 -0500

    [ Task 8 ] Try changing if [ 01/11/2022/02:27 ]

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -11,18 +11,14 @@ if __name__ == "__main__":
     from sys import argv
 
     url = "http://0.0.0.0:5000/search_user"
-    try:
-        payload = {"q": argv[1]}
-    except Exception as e:
-        payload = {"q": ""}
+    payload = {"q": argv[1]} if len(argv) > 1 else {"q": ""}
 
     response = requests.post(url, data=payload)
     try:
         data = response.json()
+        if len(data) > 0:
+            print("[{}] {}".format(data["id"], data["name"]))
+        else:
+            print("No result")
     except ValueError as e:
         print("Not a valid JSON")
-
-    if len(data) > 0:
-        print("[{}] {}".format(data["id"], data["name"]))
-    else:
-        print("No result")
