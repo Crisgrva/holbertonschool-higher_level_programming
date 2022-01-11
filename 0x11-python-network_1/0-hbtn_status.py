2a66b18019b0b045ecbbbdc7715175ecfb9e122f
Crisgrva <crstngranada@gmail.com>
2022-01-11 10:23:35 -0500

    [ Task 0 ] Function that fecth source from url and print Body response [ 01/11/2022/10:23 ]

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Write a Python script that fetches https://intranet.hbtn.io/status
+"""
+
+import urllib.request
+
+
+def get_response(url: str):
+    """
+    Function that fecth source from url
+    and return Body response
+    """
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        return(f"""Body response:
+        - type: {type(html)}
+        - content: {html}
+        - utf8 content: {html.decode('utf-8')}""")
+
+
+print(get_response("https://intranet.hbtn.io/status"))
