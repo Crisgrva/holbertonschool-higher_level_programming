0706e833d534f6e62c16be6dfa9e732eea0dcb43
Crisgrva <crstngranada@gmail.com>
2022-01-11 13:41:14 -0500

    [ Task 6 ] Add script that sends post request with parameters [ 01/11/2022/01:41 ]

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes in a URL and
+an email address, sends a POST request to
+the passed URL with the email as a parameter,
+and finally displays the body of the response.
+"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    url = argv[1]
+    email = argv[2]
+    payload = {
+        "email": email,
+    }
+
+    response = requests.post(url, data=payload)
+    print(response.text)
