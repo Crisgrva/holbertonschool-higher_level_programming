51c96cd8a11aa15d804b272a4dcd2e6e88c46249
Crisgrva <crstngranada@gmail.com>
2022-01-11 13:24:54 -0500

    [ Task 4 ] Fix spaces [ 01/11/2022/01:24 ]

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     response = requests.get(url)
     print("Body response:")
     print(f"\t- type: {type(response.text)}")
-    print(f"\t - content: {response.text}")
+    print(f"\t- content: {response.text}")
