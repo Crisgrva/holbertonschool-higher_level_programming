70f9dde0cf599fce7aa2639eb60e0e3599e9a63d
Crisgrva <crstngranada@gmail.com>
2022-01-11 10:55:11 -0500

    [ Task 0 ] Try with spaces [ 01/11/2022/10:55 ]

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """
-Write a Python script that fetches https://intranet.hbtn.io/status
+Write a Python script that
+fetches from https://intranet.hbtn.io/status
 """
+
+
 if __name__ == "__main__":
     from urllib import request
     url = "https://intranet.hbtn.io/status"
@@ -9,6 +12,6 @@ if __name__ == "__main__":
     with request.urlopen(url) as response:
         html = response.read()
         print("Body response:")
-        print(f"\t- type: {type(html)}")
-        print(f"\t- content: {html}")
-        print(f"\t- utf8 content: {html.decode('utf-8')}")
+        print(f"    - type: {type(html)}")
+        print(f"    - content: {html}")
+        print(f"    - utf8 content: {html.decode('utf-8')}")
