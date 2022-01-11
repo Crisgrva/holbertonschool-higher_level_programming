a85366900d1b4230b249bcdc11343b9a16bd8ab1
Crisgrva <crstngranada@gmail.com>
2022-01-11 10:44:26 -0500

    [ Task 0 ] Try without function [ 01/11/2022/10:44 ]

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,19 +5,12 @@ Write a Python script that fetches https://intranet.hbtn.io/status
 
 from urllib import request
 
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
 
-def get_response(url: str):
-    """
-    Function that fecth source from url
-    and return Body response
-    """
     with request.urlopen(url) as response:
         html = response.read()
-        return(f"""Body response:
-\t- type: {type(html)}
-\t- content: {html}
-\t- utf8 content: {html.decode('utf-8')}""")
-
-
-if __name__ == "__main__":
-    print(get_response("https://intranet.hbtn.io/status"))
+        print(f"""Body response:
+    - type: {type(html)}
+    - content: {html}
+    - utf8 content: {html.decode('utf-8')}""")
