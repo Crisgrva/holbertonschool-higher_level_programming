3fa6b6da66849dc4a36f12e06d6dee47671bd59c
Crisgrva <crstngranada@gmail.com>
2022-01-11 12:00:24 -0500

    [ Task 3 ] Add script that print page content or Error code if failed [ 01/11/2022/12:00 ]

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes in a URL,
+sends a request to the URL and displays the
+body of the response (decoded in utf-8).
+"""
+
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError
+from sys import argv
+
+if __name__ == "__main__":
+    req = Request(argv[1])
+    try:
+        with urlopen(req) as response:
+            page = response.read()
+            print(page.decode('utf-8'))
+    except HTTPError as e:
+        print('Error code: ', e.code)
