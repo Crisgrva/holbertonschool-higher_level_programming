d4e9004d49265cdcd8cc154e7702d66357378673
Crisgrva <crstngranada@gmail.com>
2022-01-11 13:25:51 -0500

    [ Task 4 ] Change format string [ 01/11/2022/01:25 ]

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     url = "https://intranet.hbtn.io/status"
     response = requests.get(url)
     print("Body response:")
-    print(f"\t- type: {type(response.text)}")
-    print(f"\t- content: {response.text}")
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
