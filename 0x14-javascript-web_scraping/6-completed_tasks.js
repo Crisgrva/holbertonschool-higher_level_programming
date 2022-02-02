f93ee13c02add825a122a9bb08229db1d47c02ef
Crisgrva <crstngranada@gmail.com>
2022-02-01 23:31:15 -0500

    [ Task 6 ] Come back to last commit [ 02/01/2022/11:31 ]

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -13,14 +13,18 @@ request.get(url, function (error, response, body) {
   const persons = {};
 
   body = JSON.parse(body);
+  // Creating all users
+  for (const user of body) { persons[user.userId] = []; }
+
   for (const user of body) {
-    if (user.completed) {
-      if (persons[user.userId] === undefined) {
-        persons[user.userId] = 0;
-      } else {
-        persons[user.userId]++;
-      }
+    if (user.completed === true) {
+      persons[String(user.userId)].push(user.id);
     }
   }
-  console.log(persons);
+
+  const newDict = {};
+  for (const [key, value] of Object.entries(persons)) {
+    newDict[key] = value.length;
+  }
+  console.log(newDict);
 });
