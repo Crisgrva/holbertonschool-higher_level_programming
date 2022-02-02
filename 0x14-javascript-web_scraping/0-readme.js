ce8407e9fdef35e80f795609a0db9560403092d6
Crisgrva <crstngranada@gmail.com>
2022-02-01 19:48:58 -0500

    [ Task 0 && README ] Add task 0 that reads and prints the content of a file [ 02/01/2022/07:48 ]

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// script that reads and prints the content of a file.
+const process = require('process');
+const fs = require('fs');
+
+const file = process.argv[2];
+
+fs.readFile(file, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
